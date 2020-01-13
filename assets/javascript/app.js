84ed2cdadf0bc71cbfc56d147933868caf4875ee
Jeff <jgunderson819@gmail.com>
2020-01-13 11:27:00 -0600

    Completed trivia game using ideas and structure from things I found, I may want to do more with the code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,82 @@
 // Create alert to give instructuion and start timer
 
-alert("What are the state capitals of each state?  You will have 5 minutes to get all capitals.")
+// alert("What are the state capitals of each state?  You will have 5 minutes to get all capitals.")
 
 
 
 
 
-// create countdown timer
-    //set minutes 
-    var mins = 5; 
+// // create countdown timer
+//     //set minutes 
+//     var mins = 5; 
   
-    //calculate the seconds 
-    var secs = mins * 60; 
+//     //calculate the seconds 
+//     var secs = mins * 60; 
 
-    //countdown function is evoked when page is loaded 
-    function countdown() { 
-        setTimeout('Decrement()', 60); 
-    } 
-    countdown()
-    //Decrement function decrement the value. 
-    function Decrement() { 
-        if (document.getElementById) { 
-            minutes = document.getElementById("minutes"); 
-            seconds = document.getElementById("seconds"); 
+//     //countdown function is evoked when page is loaded 
+//     function countdown() { 
+//         setTimeout('Decrement()', 60); 
+//     } 
+//     countdown()
+//     //Decrement function decrement the value. 
+//     function Decrement() { 
+//         if (document.getElementById) { 
+//             minutes = document.getElementById("minutes"); 
+//             seconds = document.getElementById("seconds"); 
 
-            //if less than a minute remaining 
-            //Display only seconds value. 
-            if (seconds < 59) { 
-                seconds.value = secs; 
-            } 
+//             //if less than a minute remaining 
+//             //Display only seconds value. 
+//             if (seconds < 59) { 
+//                 seconds.value = secs; 
+//             } 
 
-            //Display both minutes and seconds 
-            //getminutes and getseconds is used to 
-            //get minutes and seconds 
-            else { 
-                minutes.value = getminutes(); 
-                seconds.value = getseconds(); 
-            } 
-            //when less than a minute remaining 
-            //colour of the minutes and seconds 
-            //changes to red 
-            if (mins < 1) { 
-                minutes.style.color = "red"; 
-                seconds.style.color = "red"; 
-            } 
-            //if seconds becomes zero, 
-            //then page alert time up 
-            if (mins < 0) { 
-                alert('time up'); 
-                minutes.value = 0; 
-                seconds.value = 0; 
-            } 
-            //if seconds > 0 then seconds is decremented 
-            else { 
-                secs--; 
-                setTimeout('Decrement()', 1000); 
-            } 
-        } 
-    } 
+//             //Display both minutes and seconds 
+//             //getminutes and getseconds is used to 
+//             //get minutes and seconds 
+//             else { 
+//                 minutes.value = getminutes(); 
+//                 seconds.value = getseconds(); 
+//             } 
+//             //when less than a minute remaining 
+//             //colour of the minutes and seconds 
+//             //changes to red 
+//             if (mins < 1) { 
+//                 minutes.style.color = "red"; 
+//                 seconds.style.color = "red"; 
+//             } 
+//             //if seconds becomes zero, 
+//             //then page alert time up 
+//             if (mins < 0) { 
+//                 alert('time up'); 
+//                 minutes.value = 0; 
+//                 seconds.value = 0; 
+//             } 
+//             //if seconds > 0 then seconds is decremented 
+//             else { 
+//                 secs--; 
+//                 setTimeout('Decrement()', 1000); 
+//             } 
+//         } 
+//     } 
 
-    function getminutes() { 
-        //minutes is seconds divided by 60, rounded down 
-        mins = Math.floor(secs / 60); 
-        return mins; 
-    } 
+//     function getminutes() { 
+//         //minutes is seconds divided by 60, rounded down 
+//         mins = Math.floor(secs / 60); 
+//         return mins; 
+//     } 
 
-    function getseconds() { 
-        //take minutes remaining (as seconds) away  
-        //from total seconds remaining 
-        return secs - Math.round(mins * 60); 
-    } 
+//     function getseconds() { 
+//         //take minutes remaining (as seconds) away  
+//         //from total seconds remaining 
+//         return secs - Math.round(mins * 60); 
+//     } 
 
 
-// create questions
-var state =["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Conecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennesee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-
+// // create questions
+// var state =["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Conecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennesee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+Survey
+    .StylesManager
+    .applyTheme("modern");
 var json = {
     title: "State Capitals",
     showProgressBar: "bottom",
@@ -88,7 +90,7 @@ var json = {
             questions: [
                 {
                     type: "html",
-                    html: "You are about to start quiz by history. <br/>You have 10 seconds for every page and 25 seconds for the whole survey of 3 questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready."
+                    html: "You are about to start the quiz with all state capitals. <br/>You have 10 seconds for every page and 7 minutes for the whole survey of 50 questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready."
                 }
             ]
         }, {
@@ -351,19 +353,7 @@ var json = {
                 }
             ]
         },
-        {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "Alabama",
-                    title: "What is the state capital of Alabama",
-                    choices: [
-                        "Birmingham", "Mobile", "Mongomery", "Huntsville"
-                    ],
-                    correctAnswer: "Mongomery"
-                }
-            ]
-        },
+    
         {
             questions: [
                 {
@@ -767,7 +757,7 @@ survey
             .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
     });
 
-$("#surveyElement").Survey({model: survey});
+    $("#surveyElement").Survey({model: survey});
 
 
 
